8d4dd874d458043f2e31e7c40cc12c8cf334a232
rodrigueztrach <159980920+rodrigueztrach@users.noreply.github.com>
2024-05-10 22:42:53 -0600

    Add files via upload

---
+++ tipos_de_datos.py
@@ -0,0 +1,40 @@
+#tipos de datos 
+
+#tipo string 
+
+edad = 23 
+
+apellido = "rodriguez"
+
+telefono = 83428468
+
+numero_nuevo_de_telefono = 43862482
+
+print(edad)
+print(apellido)
+print(telefono)
+print(numero_nuevo_de_telefono)
+
+
+#numericos 
+
+entero = 10 
+
+flotante = 12.5 
+
+complejo = 12 + 3 
+
+
+print(entero)
+print(flotante) 
+print(complejo)
+
+
+
+#otros tipos de datos 
+
+#listas 
+
+mi_lista1 = [1,2,3,'cinco',.09,7]
+
+print (mi_lista1)
